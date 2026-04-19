f9e23cb39bb863eb23626c3445b9e8113348f584
ThanhPhuc92ps <thanhphuc92ps@gmail.com>
2026-04-19 12:29:07 +0700

    Create firebase-config.js

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyASbbhvVKV12-ToHs5Lq2EZyNjvla-ALkU",
+    authDomain: "mask-tu-vung.firebaseapp.com",
+    projectId: "mask-tu-vung",
+    storageBucket: "mask-tu-vung.firebasestorage.app",
+    messagingSenderId: "281486876910",
+    appId: "1:281486876910:web:02446860686fe9686253ff",
+    measurementId: "G-CFJHEJ2NG9"
+};
